0a993ccca1f835d4504a466f4e06ef12cf31247a
Jekarter <jkartyalta@yandex.ru>
2023-06-15 16:42:01 +0300

    feat(card-menu): add card-menu and styled main components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,87 @@
 'use client'
-import Image from 'next/image'
 import styles from './page.module.css'
-import Button from '@mui/material/Button';
-import AddCardIcon from '@mui/icons-material/AddCard';
 import { useState } from 'react';
 import BasicModal from '@/components/BasicModal';
+import { Box, Typography } from '@mui/material';
+import Visa from '../../public/visa.svg'
+import Image from 'next/image';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+const styledCard = {
+  position: 'relative',
+  width: 320,
+  height: 200,
+  borderRadius: '10%',
+  background: `linear-gradient( 45deg, transparent, rgb(170, 90, 90) ) rgb(28, 42, 55);`,
+  color: 'white',
+  textShadow: '1px 1px 2px black, 0 0 1em white, 0 0 0.2em black',
+  p: 3,
+  fontSize: 20,
+}
+
+const styledMenuCard = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  width: 550,
+  height: 100,
+  backgroundColor: '#2F4050',
+  borderRadius: 4,
+  mt: 3,
+  color: '#a7b1c2',
+  alignItems: 'end'
+}
+
+const styledMenuCardItem = {
+  textAlign: 'center',
+  fontSize: 8,
+  p: 1.8,
+  '&:hover': {
+    backgroundColor: '#293846',
+    color: '#fff'
+  }
+}
+
+const styledMenuCardIcom = {
+  fontSize: 45
+}
+
+
 
 export default function Home() {
-  const [haveACard, setHaveACard] = useState(false)
+  const [haveACard, setHaveACard] = useState(true)
 
   return (
     <main className={styles.main}>
-      <div>
-        {!haveACard ?
-        <BasicModal/> :
-        <div>Здесь будет карта</div>} 
-      </div>
+      <Box>
+        {haveACard ?
+          <Box sx={styledCard}>
+            <Box>TurkaPay</Box>
+            <Box sx={{mt: 9}}>0000 0000 0000 0001</Box>
+            <Box sx={{position: 'absolute', ml: 'auto', left: 225, bottom: -10}}>
+              <Image src={Visa} alt='visa' width={80} height={80}/>
+            </Box>
+            
+          </Box> :
+          <BasicModal/>
+        } 
+      </Box>
+      <Box sx={styledMenuCard}>
+        <Box sx={styledMenuCardItem}>
+          <AddCircleIcon sx={styledMenuCardIcom} />
+          <Typography>Пополнить счёт</Typography>
+        </Box>
+        <Box sx={styledMenuCardItem} >
+          <AccessTimeFilledIcon sx={styledMenuCardIcom} />
+          <Typography>Увеличить время</Typography>
+        </Box>
+        <Box sx={styledMenuCardItem}>
+          <CancelIcon sx={styledMenuCardIcom} />
+          <Typography>Закрыть карту</Typography>
+        </Box>
+          
+      </Box>
     </main>
   )
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,23 +3,37 @@ import * as React from 'react';
 import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
 import Link from 'next/link';
-import { Drawer } from '@mui/material';
+import { Box, Drawer, Typography } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import HistoryIcon from '@mui/icons-material/History';
+
+const styledMenuItem = {
+  color: '#a7b1c2',
+  '&:hover': {
+    backgroundColor: '#293846',
+    color: '#fff'
+  }
+}
 
 const Navigation = () => {
 
   return (
-    <Drawer variant='permanent' >
-      <MenuList sx={{ width: 250 }}>
+    <Drawer variant='permanent'   PaperProps={{
+      sx: {
+        backgroundColor: "#2F4050",
+        color: "#fff",
+      }
+    }} >
+      <Typography variant='h3' sx={{backgroundColor: '#2F4050', borderBottom: '1px solid gray', padding: 2, fontSize: 20}}>TurkaPay</Typography>
+      <MenuList sx={{backgroundColor: '#2F4050', width: 250 }}>
         <Link href='/'>
-          <MenuItem>
-            <HomeIcon sx={{ mx: 1}}  />Главная
+          <MenuItem  sx={styledMenuItem}>
+            <HomeIcon sx={{ m: 1 }} />Главная
           </MenuItem>
         </Link>
         <Link href='/history'>
-          <MenuItem>
-          <HistoryIcon sx={{ mx: 1}} /> История
+          <MenuItem sx={styledMenuItem} >
+          <HistoryIcon sx={{ m: 1 }} /> История
           </MenuItem>
         </Link>
       </MenuList>
